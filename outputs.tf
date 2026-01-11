74bb5809d291cb16e377b345269830eadcd2e9e1
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:50:25 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "app_service_certificate_bindings" {
+  description = "All app_service_certificate_binding resources"
+  value       = azurerm_app_service_certificate_binding.app_service_certificate_bindings
+}
+
