fb541ce530ec91b670b57e90f894afe726a6e5cd
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:40 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,25 @@
-output "app_service_certificate_bindings" {
-  description = "All app_service_certificate_binding resources"
-  value       = azurerm_app_service_certificate_binding.app_service_certificate_bindings
-}
 output "app_service_certificate_bindings_app_service_name" {
-  description = "List of app_service_name values across all app_service_certificate_bindings"
-  value       = [for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : v.app_service_name]
+  description = "Map of app_service_name values across all app_service_certificate_bindings, keyed the same as var.app_service_certificate_bindings"
+  value       = { for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : k => v.app_service_name }
 }
 output "app_service_certificate_bindings_certificate_id" {
-  description = "List of certificate_id values across all app_service_certificate_bindings"
-  value       = [for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : v.certificate_id]
+  description = "Map of certificate_id values across all app_service_certificate_bindings, keyed the same as var.app_service_certificate_bindings"
+  value       = { for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : k => v.certificate_id }
 }
 output "app_service_certificate_bindings_hostname" {
-  description = "List of hostname values across all app_service_certificate_bindings"
-  value       = [for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : v.hostname]
+  description = "Map of hostname values across all app_service_certificate_bindings, keyed the same as var.app_service_certificate_bindings"
+  value       = { for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : k => v.hostname }
 }
 output "app_service_certificate_bindings_hostname_binding_id" {
-  description = "List of hostname_binding_id values across all app_service_certificate_bindings"
-  value       = [for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : v.hostname_binding_id]
+  description = "Map of hostname_binding_id values across all app_service_certificate_bindings, keyed the same as var.app_service_certificate_bindings"
+  value       = { for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : k => v.hostname_binding_id }
 }
 output "app_service_certificate_bindings_ssl_state" {
-  description = "List of ssl_state values across all app_service_certificate_bindings"
-  value       = [for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : v.ssl_state]
+  description = "Map of ssl_state values across all app_service_certificate_bindings, keyed the same as var.app_service_certificate_bindings"
+  value       = { for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : k => v.ssl_state }
 }
 output "app_service_certificate_bindings_thumbprint" {
-  description = "List of thumbprint values across all app_service_certificate_bindings"
-  value       = [for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : v.thumbprint]
+  description = "Map of thumbprint values across all app_service_certificate_bindings, keyed the same as var.app_service_certificate_bindings"
+  value       = { for k, v in azurerm_app_service_certificate_binding.app_service_certificate_bindings : k => v.thumbprint }
 }
 
